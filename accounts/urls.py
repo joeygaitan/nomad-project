eae4400baad27949c9acd858f66fc1719a383bab
Cao Mai <caomai@rocketmail.com>
2020-03-02 11:19:00 -0800

    [add] account page

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from accounts.views import SignUpView, Sayhi, update_profile, ProfilesListView, Homepage, GetRiders
+from accounts.views import SignUpView, Sayhi, update_profile, ProfilesListView, Homepage, GetRiders, login_view_profile
 
 
 from . import views
@@ -10,5 +10,6 @@ urlpatterns = [
     path('say_hi/', Sayhi.as_view(), name="sayhi-page"),
     path('get_ride/', update_profile, name='get-ride-page'),
     path('get_riders/', GetRiders.as_view(), name='get-riders-page'),
-    path('all_rides/', ProfilesListView.as_view(), name='list-rides-page')
+    path('all_rides/', ProfilesListView.as_view(), name='list-rides-page'),
+    path('profile/', login_view_profile, name='account-page')
 ]
